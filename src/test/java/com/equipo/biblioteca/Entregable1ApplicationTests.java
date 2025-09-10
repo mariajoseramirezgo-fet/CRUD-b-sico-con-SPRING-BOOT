08ad71ac15e907e59921966b3d2cdaf280b6586e
MariaJose <mariajose_ramirezgo@fet.edu.co>
2025-09-10 15:05:51 -0500

    Primer commit

---
+++ src/test/java/com/equipo/biblioteca/Entregable1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Entregable1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
