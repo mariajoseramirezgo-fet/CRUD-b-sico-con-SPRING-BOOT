e1771bb6b58cddd8c13ec129ec8f58be32d6280b
Harold Smith López Forero <harold_lopezfo@fet.edu.co>
2025-09-18 23:50:25 -0500

    agregar un listado

--- src/main/java/com/equipo/Entregable1Application.java
+++ src/main/java/com/equipo/Entregable1Application.java
@@ -1,4 +1,4 @@
-package src.main.java.com.equipo;
+package com.equipo.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
